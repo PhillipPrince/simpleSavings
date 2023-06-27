43ac2c26b9455c1207118a4c4eb90774dbefe8c0
PhillipPrince <68793926+PhillipPrince@users.noreply.github.com>
2023-06-27 18:34:06 +0300

    commit

--- Simple_Saving_API/src/main/java/com/simpleSavings/Simple_Saving_API/controller/ProductController.java
+++ Simple_Saving_API/src/main/java/com/simpleSavings/Simple_Saving_API/controller/ProductController.java
@@ -1,4 +1,5 @@
 package com.simpleSavings.Simple_Saving_API.controller;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,13 @@ import com.simpleSavings.Simple_Saving_API.model.Product;
 import com.simpleSavings.Simple_Saving_API.request.ProductRequest;
 import com.simpleSavings.Simple_Saving_API.services.ProductService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("/api/products")
+@Api(tags = "Products")
 public class ProductController {
 
     private final ProductService productService;
@@ -22,17 +28,24 @@ public class ProductController {
     }
 
     @PostMapping("/createProduct")
-    public ResponseEntity<Product> createProduct(@Valid @RequestBody ProductRequest request) {
+    @ApiOperation(value = "Create a new product")
+    public ResponseEntity<Product> createProduct(
+            @Valid @RequestBody ProductRequest request
+    ) {
         Product product = productService.createProduct(request);
-        if(product==null) {
-        	return ResponseEntity.badRequest().build();
-        	
+        if (product == null) {
+            return ResponseEntity.badRequest().build();
         }
         return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 
     @GetMapping("/getProduct/{productId}")
-    public ResponseEntity<Product> getProduct(@PathVariable Long productId) {
+    @ApiOperation(value = "Get a product by ID")
+    public ResponseEntity<Product> getProduct(
+            @PathVariable
+            @ApiParam(value = "Product ID", example = "1")
+                    Long productId
+    ) {
         Product product = productService.getProduct(productId);
         if (product != null) {
             return ResponseEntity.ok(product);
@@ -41,21 +54,28 @@ public class ProductController {
         }
     }
 
-    
     @PatchMapping("/update/{productId}")
+    @ApiOperation(value = "Update a product by ID")
     public ResponseEntity<Product> updateProduct(
-            @PathVariable Long productId,
+            @PathVariable
+            @ApiParam(value = "Product ID", example = "1")
+                    Long productId,
             @RequestBody ProductRequest request
     ) {
         Product product = productService.updateProduct(productId, request);
-        if(product==null) {
-        	return ResponseEntity.badRequest().build();
+        if (product == null) {
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(product);
     }
 
     @DeleteMapping("/delete/{productId}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+    @ApiOperation(value = "Delete a product by ID")
+    public ResponseEntity<Void> deleteProduct(
+            @PathVariable
+            @ApiParam(value = "Product ID", example = "1")
+                    Long productId
+    ) {
         productService.deleteProduct(productId);
         return ResponseEntity.noContent().build();
     }
--- Simple_Saving_API/src/main/java/com/simpleSavings/Simple_Saving_API/controller/TransactionController.java
+++ Simple_Saving_API/src/main/java/com/simpleSavings/Simple_Saving_API/controller/TransactionController.java
@@ -14,8 +14,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("/api/customers/transactions")
+@Api(tags = "Transactions")
 public class TransactionController {
 
     private final TransactionService transactionService;
@@ -25,36 +30,42 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
-    
     @PostMapping("/{customerId}/create")
+    @ApiOperation(value = "Create a new transaction for a customer")
     public ResponseEntity<Transaction> createTransaction(
-            @PathVariable Long customerId,
+            @PathVariable
+            @ApiParam(value = "Customer ID", example = "1")
+                    Long customerId,
             @RequestBody TransactionRequest request
     ) {
         Transaction transaction = transactionService.createTransaction(customerId, request);
-        if(transaction==null) {
-        	return ResponseEntity.badRequest().build();	
+        if (transaction == null) {
+            return ResponseEntity.badRequest().build();
         }
         return new ResponseEntity<>(transaction, HttpStatus.CREATED);
     }
-    
-    @GetMapping("/customerId/total-savings")
-    public ResponseEntity<Double> getTotalSavingsAmount(@PathVariable Long customerId) {
+
+    @GetMapping("/{customerId}/total-savings")
+    @ApiOperation(value = "Get the total savings amount for a customer")
+    public ResponseEntity<Double> getTotalSavingsAmount(
+            @PathVariable
+            @ApiParam(value = "Customer ID", example = "1")
+                    Long customerId
+    ) {
         Double totalSavingsAmount = transactionService.getTotalSavingsAmount(customerId);
-        if(totalSavingsAmount==null) {
-        	return ResponseEntity.badRequest().build();
+        if (totalSavingsAmount == null) {
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(totalSavingsAmount);
     }
-    
+
     @GetMapping("/total-savings")
+    @ApiOperation(value = "Get the total savings accounts")
     public ResponseEntity<List<Transaction>> getTotalSavingsAccount() {
         List<Transaction> totalSavingsAmount = transactionService.getTotalSavingsAccount();
-        if(totalSavingsAmount==null) {
-        	return ResponseEntity.badRequest().build();
+        if (totalSavingsAmount == null) {
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(totalSavingsAmount);
     }
-
-
 }
